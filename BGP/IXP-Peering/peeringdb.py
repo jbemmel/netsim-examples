c1be83fd2c572f95aef121945f4ba7b27b935585
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-07 10:59:18 -0500

    Different structure, all peers in single LAN

--- BGP/IXP-Peering/peeringdb.py
+++ BGP/IXP-Peering/peeringdb.py
@@ -6,13 +6,6 @@ from box import Box
 from netsim import data
 
 """
-Adds a custom node ixp attribute (marks an IXP where peering is taking place)
-"""
-def init(topology: Box) -> None:
-    topology.defaults.attributes.node.append('ixp')
-    topology.defaults.bgp.attributes.interface.append('multihop')
-
-"""
 Lookup ASN in PeeringDB and return ipv4,ipv6 peering IPs at given IX
 """
 def query_peeringdb(asn: int, ix: str) -> typing.Tuple[typing.Optional[str],typing.Optional[str]]:
@@ -27,35 +20,48 @@ def query_peeringdb(asn: int, ix: str) -> typing.Tuple[typing.Optional[str],typi
   return ( None, None )
 
 """
-Convert eBGP peerings with IXP to multihop peerings between connected nodes
+Update eBGP peering IP addresses at given IXP site
 """
 def post_transform(topology: Box) -> None:
 
+  # Iterate over all IXP nodes, update interface IP addresses based on PeeringDB
+  for n, ndata in topology.nodes.items():
+    print( f"Processing {n} bgp.neighbors" )
+    updated_neighbors = []
+    for i in ndata.bgp.neighbors:
+      print(i)
+      def update_ips(ip,prefix,ipv):
+        net = netaddr.IPNetwork( f"{ip}/{prefix}" )
+        ixp_ip = str( net[1] )
+        ndata.interfaces[i.ifindex-1][ipv] = f"{ixp_ip}/{prefix}"
+        for nb in ndata.interfaces[i.ifindex-1].neighbors:
+          peer = topology.nodes[ nb.node ]
+          intf = [ l for l in peer.interfaces if l.ifname == nb.ifname ]
+          if intf:
+            intf[0][ipv] = f"{ip}/{prefix}"
 
-
-    # Iterate over all IXP nodes, update interface IP addresses based on PeeringDB
-    ixp = topology.get('ixp',"AMS-IX")
-    for n, ndata in topology.nodes.items():
       if 'ixp' in ndata:
-        for i in ndata.bgp.neighbors:
+        (ip4,ip6) = query_peeringdb( i['as'], n )
+        if ip4 and 'ipv4' in ndata.af:
+          update_ips(ip4,22,'ipv4')    # Assume /22 is large enough
+        if ip6 and 'ipv6' in ndata.af:
+          update_ips(ip6,64,'ipv6')
 
-          def update_ips(ip,prefix,ipv):
-            net = netaddr.IPNetwork( f"{ip}/{prefix}" )
-            ip0, ip1 = [ str(i) for i in list(net) ]
-            ixp_ip = ip0 if ip1==ip else ip1
+        ndata.pop('bgp',None) # Remove BGP peerings from IXP node
+      else:
+        # Assumes IXP nodes are listed first...update peering IPs
+        peer = topology.nodes[ i.name ]
+        if 'ixp' in peer:
+          continue
+        for l in peer.interfaces:
+          print(l)
+          if l.ifname == ndata.interfaces[ i.ifindex-1 ].ifname:
+            print( f"Found peering interface {l}" )
+            if 'ipv4' in l and 'ipv4' in i:
+              i.ipv4 = l.ipv4
+            if 'ipv6' in l and 'ipv6' in i:
+              i.ipv6 = l.ipv6
+            updated_neighbors = [ i ]  # Keep only this one
 
-            ndata.interfaces[i.ifindex-1][ipv] = f"{ixp_ip}/{prefix}"
-            for nb in ndata.interfaces[i.ifindex-1].neighbors:
-              peer = topology.nodes[ nb.node ]
-              intf = [ l for l in peer.interfaces if l.ifname == nb.ifname ]
-              if intf:
-                intf[0][ipv] = f"{ip}/{prefix}"
-                intf[0].bgp.multihop = True
-
-          (ip4,ip6) = query_peeringdb( i['as'], n )
-          if ip4 and 'ipv4' in ndata.af:
-            update_ips(ip4,31,'ipv4')
-          if ip6 and 'ipv6' in ndata.af:
-            update_ips(ip6,127,'ipv6')
-
-        ndata.pop('bgp',None)
+    if updated_neighbors:
+      ndata.bgp.neighbors = updated_neighbors
